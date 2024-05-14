1bffd085802f8d12216057c3a21b1819376ed987
Ayushman200 <119736281+Ayushman200@users.noreply.github.com>
2024-05-14 22:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let clockSound = new Audio("tik-tok.mp3");
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
+    clockSound.play();
+},1000);
